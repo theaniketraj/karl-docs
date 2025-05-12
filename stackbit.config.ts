016b4676128080ca9159f7711e64fe7ec812231f
Aniket Raj <118242466+theaniketraj@users.noreply.github.com>
2025-05-13 02:11:03 +0530

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -7,3 +7,25 @@ export default defineStackbitConfig({
     "contentSources": [],
     "postInstallCommand": "npm i --no-save @stackbit/types"
 })
+
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  // ...
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          // Define the model as a page model
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
+    })
+  ]
+});
